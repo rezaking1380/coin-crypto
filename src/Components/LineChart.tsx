412d896512a6a6ba318785ac32e61ddd4a90c4ea
REZA <reza.ngr8@gmail.com>
2023-02-17 04:10:18 -0800

    update

--- src/Components/LineChart.tsx
+++ src/Components/LineChart.tsx
@@ -2,55 +2,54 @@ import { Col, Row, Typography } from "antd";
 import millify from "millify";
 import React from "react";
 import { Line } from "react-chartjs-2";
-import {CategoryScale} from 'chart.js'; 
-import Chart from 'chart.js/auto';
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
 import moment from "moment";
 
+const { Title } = Typography;
 
-
-const {Title} = Typography
-
-const LineChart = ({coinHistory, currentPrice ,coinName}:any) => {
-    Chart.register(CategoryScale);
-  const coinPrice = []
-  const coinTimesTamp = []
+const LineChart = ({ coinHistory, currentPrice, coinName }: any) => {
+  Chart.register(CategoryScale);
+  const coinPrice = [];
+  const coinTimesTamp = [];
   for (let i = 0; i < coinHistory.data.history.length; i++) {
-    coinPrice.push(coinHistory.data.history[i].price)
-    coinTimesTamp.push(new Date(coinHistory.data.history[i].timestamp * 1000).toLocaleDateString())
+    coinPrice.push(coinHistory.data.history[i].price);
+    coinTimesTamp.push(
+      new Date(
+        coinHistory.data.history[i].timestamp * 1000
+      ).toLocaleDateString()
+    );
   }
   const data = {
     labels: coinTimesTamp,
     datasets: [
       {
-        label: 'Price in USD',
+        label: "Price in USD",
         data: coinPrice,
-        borderColor: '#0071bd',
-        backgroundColor: '#0071bd',
-        fill: false
-      }
-    ]
+        borderColor: "#0071bd",
+        backgroundColor: "#0071bd",
+        fill: false,
+      },
+    ],
   };
-  const option ={
-    scales: {
-        yAxis: [
-            {
-                ticks: {
-                    beginAtZero: true
-                }
-            }
-        ]
-    }
-  }
-  return <div>
-     <Row className='chart-header'>
-        <Title level={2} className='chart-title'>{coinName} Price Chart</Title>
+  return (
+    <div>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart
+        </Title>
         <Col className="price-container">
-            <Title level={5} className='price-change'>Change in time : {coinHistory.data.change} %</Title>
-            <Title level={5} className='current-price'>Current {coinName} Price: $ {millify(Number(currentPrice))}</Title>
+          <Title level={5} className="price-change">
+            Change in time : {coinHistory.data.change} %
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {millify(Number(currentPrice))}
+          </Title>
         </Col>
-     </Row>
-     <Line data={data} options={option} />
-  </div>;
+      </Row>
+      <Line data={data} />
+    </div>
+  );
 };
 
 export default LineChart;
